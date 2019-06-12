aa8312dcd30d148680c452dd06aeb50f76d54fb4
kemalaraz <45275051+kemalaraz@users.noreply.github.com>
2019-06-12 22:17:03 +0300

    Create Airport.py

---
+++ Airport.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+
+class Airport:
+
+    def __init__(self,airport_id,name,city,country,code,icacode,lat,long,alt,toffset,DST,Tz):
+
+        self.airport_id=int(airport_id)
+        self.name=name
+        self.city=city
+        self.country=country
+        self.code=code
+        self.icacode=icacode
+        self.lat=float(lat)
+        self.long=float(long)
+        self.alt=float(alt)
+        self.toffset=float(toffset)
+        self.DST=DST
+        self.Tz=Tz
+
+    def getCoordinates(self):
+
+        """
+        gets the IATA code and returns the latitude
+        and longitude related with that IATA code
+
+        **Parameters**\n
+        ----------\n
+        self: IATA code
+
+        **Returns**\n
+        -------\n
+        self.lat: latitude
+        self.long: longitude
+        """
+
+
+        return self.lat, self.long
+
+    def getAirportCountry(self):
+
+        """
+        gets the IATA code and returns the country
+        of that airport.
+
+        **Parameters**\n
+        ----------\n
+        self: IATA code
+
+        **Returns**\n
+        -------\n
+        self.country: country of the airport
+        """
+        return self.country
